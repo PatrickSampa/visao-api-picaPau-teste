f2dcb5609d7b12d167f1e826a34b11bc60e6e431
PatrickSampa <sampa.mendonca@gmail.com>
2023-03-10 11:01:42 -0300

    testando port

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "./app";
 import dotenv from 'dotenv';
-const PORT = process.env.API_PORT;
+
+const port = process.env.PORT || 3000;
 
 
 
 app.get('/', (req, res) => res.send('Hello World 100!'))
 
-app.listen(PORT, () => console.log("Visao runing in PORT " + PORT));
+app.listen(port, () => console.log("Visao runing in PORT " + port));
